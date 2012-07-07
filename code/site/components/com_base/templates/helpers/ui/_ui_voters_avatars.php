05d56a2075a843d9e805e27c0019eb8a3cb4af77
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2012-07-07 10:23:11 +0000

    added popup styling for a list of actor entities
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@15033 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_base/templates/helpers/ui/_ui_voters_avatars.php
+++ code/site/components/com_base/templates/helpers/ui/_ui_voters_avatars.php
@@ -4,9 +4,25 @@
 	<h3><?= $entity->voteUpCount == 1 ? @text('LIB-AN-VOTE-ONE-VOTED') : sprintf(@text('LIB-AN-VOTE-OTHER-VOTED'), $entity->voteUpCount)?></h3>
 </div>
 <div class="popup-body">
-	<div class="media-grid" data-behavior="Scrollable" data-scrollable-container="!.popover-content">
+	<div class="popup-entities an-actors">
 		<?php foreach($entity->voteups->voter as $actor) : ?>
-		<div><?= @avatar($actor) ?></div>	
+		<div class="an-entity">		
+			<div class="entity-thumbnail">
+				<?= @avatar($actor) ?>
+			</div>
+			<div class="entity-container">
+				<h4 class="entity-title"><?= @name($actor) ?></h4>
+				
+				<div class="an-meta">
+					<?= $actor->followerCount ?> 
+					<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-FOLLOWERS') ?></span> 
+				</div>
+				
+				<div class="entity-description">
+					<?= @helper('text.truncate', strip_tags($actor->description), array('length'=>200)); ?>
+				</div>
+			</div>
+		</div>
 		<?php endforeach; ?>
 	</div>
 </div>
