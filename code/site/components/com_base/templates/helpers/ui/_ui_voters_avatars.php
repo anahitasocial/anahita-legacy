b142076651e01db9c9d28a1cc7fcab5a67d9e466
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2012-07-07 07:03:05 +0000

    actor pop over scrolls if too large
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@15022 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_base/templates/helpers/ui/_ui_voters_avatars.php
+++ code/site/components/com_base/templates/helpers/ui/_ui_voters_avatars.php
@@ -4,7 +4,7 @@
 	<?= $entity->voteUpCount == 1 ? @text('LIB-AN-VOTE-ONE-VOTED') : sprintf(@text('LIB-AN-VOTE-OTHER-VOTED'), $entity->voteUpCount)?>
 </div>
 <div class="popover-content">
-	<div class="media-grid">
+	<div class="media-grid" data-behavior="Scrollable" data-scrollable-container="!.popover-content">
 		<?php foreach($entity->voteups->voter as $actor) : ?>
 		<div><?= @avatar($actor) ?></div>	
 		<?php endforeach; ?>
