098cf445879c1053cb623afee65daf1bd7700466
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2012-09-04 21:47:35 +0000

    Offline message has been styled
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@15560 a933e228-e664-4d3f-8241-49479697c698

--- code/site/templates/base/views/html/offline.php
+++ code/site/templates/base/views/html/offline.php
@@ -7,44 +7,35 @@
     <body>
         <script src="media://lib_anahita/js/min/site.js"></script>
         <div class="navbar <?= ($this->getView()->getParams()->navbarInverse) ? 'navbar-inverse' : '' ?> navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container">
-                    <?= @render('logo') ?>                    
-                </div>
-            </div>            
-        </div>        
+        	<div class="navbar-inner">
+        		<div class="container">
+        			<?= @render('logo') ?>
+        		</div>
+        	</div>
+        </div>
+   
             <div class="container">
                 <div class="row"> 
-                    <div class="span8 offset2">               
+                    <div class="span12">               
                         <div class="hero-unit">
+                        	<h1>Service Offline</h1>
                             <p><?= JFactory::getApplication()->getCfg('offline_message'); ?></p>
+                        
+                        <form data-behavior="FormValidator" class="well form-inline" action="<?= @route() ?>" method="post">
+                        	<input type="hidden" name="remember" value="yes">
+                            <input type="hidden" name="option" value="com_user">
+                            <input type="hidden" name="task" value="login">
+                            <input type="hidden" name="return" value="<?= base64_encode(JURI::base()) ?>" />
+                            <?= JHTML::_( 'form.token' ); ?>
+                            <input data-behavior="required" type="text" name="username" class="input-medium" placeholder="<?= @text('USERNAME') ?>">
+                            <input data-behavior="required" type="password" name="passwd" class="input-medium" placeholder="<?= @text('PASSWORD') ?>">
+                            <button type="submit" class="btn btn-primary"><?=@text('LOGIN')?></button>
+                        </form>
                         </div>
                     </div>
                 </div>
-                <div class="row" id="row-main">
-                    <div class="span4 offset4">
-                    <form data-behavior="FormValidator" class="well form-inline" action="<?=@route()?>" method="post">
-                        <fieldset>
-                            <legend><?= @text('LOGIN') ?></legend>
-                                        
-                                <input type="hidden" name="remember" value="yes">
-                                <input type="hidden" name="option" value="com_user">
-                                <input type="hidden" name="task" value="login">
-                                <input type="hidden" name="return" value="<?= base64_encode(JURI::base()) ?>" />
-                                <?php echo JHTML::_( 'form.token' ); ?>     
-                                <?= @helper('ui.form', array(
-                                    'USERNAME'          => @html('textfield',     'username', '')->dataValidators('required'),
-                                    'PASSWORD'          => @html('passwordfield', 'passwd',   '')->dataValidators('required')
-                                ))?>
-                                <div class="form-actions">
-                                    <button type="submit" class="btn btn-primary"><?=@text('LOGIN')?></button>
-                                </div>          
-                        </fieldset>
-                    </form>
-                    </div>
-                </div>
-            </div>       
-        
+            </div>
+            
         <?= @render('analytics') ?>
     </body>
 </html>
