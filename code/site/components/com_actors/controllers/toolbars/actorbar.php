2b71cb9f716afc205859e18ee147d696f8971f03
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2012-09-07 06:36:16 +0000

    cleaned up actor bar the untranslated tag fallbacks to the com_actors language
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@15591 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_actors/controllers/toolbars/actorbar.php
+++ code/site/components/com_actors/controllers/toolbars/actorbar.php
@@ -39,95 +39,109 @@ class ComActorsControllerToolbarActorbar extends ComBaseControllerToolbarActorba
     public function onBeforeControllerGet(KEvent $event)
     {
         parent::onBeforeControllerGet($event);
+    
+        //just in case
+        if ( !$this->getController()->isIdentifiable() ) {
+            return;
+        }
         
-        $data 	= $event->data;
-        $viewer = get_viewer();
-        $actor	= pick($this->getController()->actor, $viewer);
-        $entity = $this->getController()->getItem();
-        $layout = pick($this->getController()->layout, 'default');
-        $name	= $this->getController()->getIdentifier()->name;
-    
         //if vieweing one actor
-        if ( $entity && $this->getController()->isIdentifiable() && $entity->isDescribable() )
+        if ( $item = $this->getController()->getItem()  )
         {
-            $this->setActor(null);
-            
-            //viewing one's socialgraph
-            if ( $this->getController()->get == 'graph' )
-            {
-                $types = array();
-    
-                if ( $entity->isFollowable() ) {
-                    $types[] = 'Followers';
-                }
-    
-                if ( $entity->isLeadable() ) {
-                    $types[] = 'Leaders';
-                    $types[] = 'Mutuals';
-                    if ( !$entity->eql( get_viewer() ) ) {
-                        $types[] = 'CommonLeaders';
-                    }
-                }
-    
-                if ( $entity->authorize('administration', array('strict'=>true)) ) {
-                    $types[] = 'Blockeds';
-                }
-
-                foreach($types as $type)
-                {
-                    $label	 = array(strtoupper('COM-'.$this->getIdentifier()->package.'-NAV-LINK-SOCIALGRAPH-'.$type));
-                    $label[] = 'COM-ACTORS-NAV-LINK-SOCIALGRAPH-'.strtoupper($type);
-                    $cmd	 = strtolower($this->getController()->sanitize($type, 'cmd'));
-                    $this->addNavigation('navbar-'.$cmd,translate($label),
-                            $entity->getURL().'&get=graph&type='.$cmd,
-                            $this->getController()->type == $cmd);
-                }
-    
-                $title  	= array(strtoupper('COM-'.$this->getIdentifier()->package.'-NAV-TITLE-SOCIALGRAPH'));
-                $title[] = 'COM-ACTORS-NAV-TITLE-SOCIALGRAPH';
-                $this->setTitle(sprintf(translate($title), $entity->name));
-                $this->setActor($entity);
+            //for now render actor socialgraph
+            //bar here
+            if ( $this->getController()->get == 'graph' ) {
+                $this->_configureGraphBar();
+            } else {
+                $this->setActor(null);
+                $this->setTitle(sprintf(JText::_('COM-ACTORS-PROFILE-ACTORBAR-TITLE'), $item->getName()));    
             }
-            else {                
-                $this->setTitle(sprintf(JText::_('COM-ACTORS-PROFILE-ACTORBAR-TITLE'), $entity->getName()));
-            }            
         }
         //if viewing a list of actors related to another actor
-        elseif ( $this->getController()->isOwnable() && $this->getController()->actor )
-        {
-            $filters = array('following');
-    
-            if ( $this->getController()->getRepository()->hasBehavior('administrable') ) {
-                $filters[] = 'administering';
-            }
-    
-            $this->setActor($this->getController()->actor);
-    
-            $type  		= ucfirst(KInflector::pluralize($this->getController()->getIdentifier()->name));
-            foreach($filters as $filter)
-            {
-                //COM-[COMPONENT]-NAV-FILTER-[ADMINISTRATING|FOLLOWING]
-                $label = array(strtoupper('COM-'.$this->getIdentifier()->package.'-NAV-FILTER-'.$filter));
-                if ( $filter == 'following' ) {
-                    $label[] = 'Following';
-                } elseif ( $filter == 'administering' ) {
-                    $label[] = 'Administering';
-                }
-                $this->addNavigation('navbar-'.$filter,translate($label),
-                        array('option'=>$this->getController()->option,'view'=>$this->getController()->view,'oid'=>$this->getController()->actor->id,'filter'=>$filter),
-                        $this->getController()->filter == $filter);
-            }
-    
-            $filter = pick($this->getController()->filter, 'following');
-            $title  = array(strtoupper('COM-'.$this->getIdentifier()->package.'-NAV-TITLE-'.$this->getController()->filter));
-    
-            if ( $filter == 'following' ) {
-                $title[] = $type.'COM-GROUPS-NAV-TITLE-FOLLOWING';
-            } elseif ( $filter == 'administering' ) {
-                $title[] = $type.'COM-GROUPS-NAV-TITLE-ADMINISTERING';
-            }
-    
-            $this->setTitle(sprintf(translate($title), $this->getController()->actor->name));
+        elseif ( $this->getController()->isOwnable() && $this->getController()->actor ) {
+            $this->_configureBar();                        
         }
-    }    
+    } 
+    
+    /**
+     * Configure actorbar for when socialgraph is showing. This is temporary as the 
+     * socialgraph should be moved into its own component
+     * 
+     * @return void
+     */
+    protected function _configureGraphBar()
+    {   
+        $entity = $this->getController()->getItem();
+        $types  = array();
+
+        if ( $entity->isFollowable() ) {
+            $types[] = 'Followers';
+        }
+
+        if ( $entity->isLeadable() ) {
+            $types[] = 'Leaders';
+            $types[] = 'Mutuals';
+            if ( !$entity->eql( get_viewer() ) ) {
+                $types[] = 'CommonLeaders';
+            }
+        }
+
+        if ( $entity->authorize('administration', array('strict'=>true)) ) {
+            $types[] = 'Blockeds';
+        }
+
+        foreach($types as $type)
+        {
+            $label   = array(strtoupper('COM-'.$this->getIdentifier()->package.'-NAV-LINK-SOCIALGRAPH-'.$type));
+            $label[] = 'COM-ACTORS-NAV-LINK-SOCIALGRAPH-'.strtoupper($type);
+            $cmd     = strtolower($this->getController()->sanitize($type, 'cmd'));
+            $this->addNavigation('navbar-'.$cmd,translate($label),
+                    $entity->getURL().'&get=graph&type='.$cmd,
+                    $this->getController()->type == $cmd);
+        }
+
+        $title      = array(strtoupper('COM-'.$this->getIdentifier()->package.'-NAV-TITLE-SOCIALGRAPH'));
+        $title[] = 'COM-ACTORS-NAV-TITLE-SOCIALGRAPH';
+        $this->setTitle(sprintf(translate($title), $entity->name));
+        $this->setActor($entity);
+    }
+    
+    /**
+     * Configure actorbar for when a list of actor's related to another is showing
+     * 
+     * @return void
+     */
+    protected function _configureBar()
+    {
+        $filters = array('following');
+
+        if ( $this->getController()->getRepository()->hasBehavior('administrable') ) {
+            $filters[] = 'administering';
+        }
+
+        $this->setActor($this->getController()->actor);
+
+        //$type  = ucfirst(KInflector::pluralize($this->getController()->getIdentifier()->name));
+        foreach($filters as $filter)
+        {
+            //COM-[COMPONENT]-NAV-FILTER-[ADMINISTRATING|FOLLOWING]
+            $label = array(
+                strtoupper('COM-ACTORS-NAV-FILTER-'.$filter),
+                strtoupper('COM-'.$this->getIdentifier()->package.'-NAV-FILTER-'.$filter)
+            );
+            
+            $this->addNavigation('navbar-'.$filter,translate($label),
+                    array('option'=>$this->getController()->option,'view'=>$this->getController()->view,'oid'=>$this->getController()->actor->id,'filter'=>$filter),
+                    $this->getController()->filter == $filter);
+        }
+
+        $filter = pick($this->getController()->filter, 'following');
+        
+        $title  = array(
+            strtoupper('COM-ACTORS-NAV-TITLE-'.$filter),        
+            strtoupper('COM-'.$this->getIdentifier()->package.'-NAV-TITLE-'.$filter)
+        );               
+        
+        $this->setTitle(sprintf(translate($title), $this->getController()->actor->name));        
+    }
 }
