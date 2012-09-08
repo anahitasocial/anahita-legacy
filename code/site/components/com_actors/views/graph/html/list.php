ae2e976c9a9bddb35839761c55e88607c917548d
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2012-09-08 01:12:16 +0000

    cleaned up some code
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@15606 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_actors/views/graph/html/list.php
+++ code/site/components/com_actors/views/graph/html/list.php
@@ -1,8 +1,14 @@
 <?php defined('KOOWA') or die; ?>
 
+<?php 
+    $url = $item->getURL().'&layout=list&get=graph&type='.$type;
+    if ( !empty($q) ) {
+        $url .= '&q='.$q;   
+    }
+?>
 <?php
     $config = array(              
-        'pagination'=> @pagination($this->getView()->getState()->getList(), array('url'=>@route('layout=list&get=graph&type='.$type.'&view='.@listItemView()->getName().'&id='.$item->id)))        
+        'pagination'=> @pagination($this->getView()->getState()->getList(), array('url'=>@route($url)))        
     );
 if ( $item->isAdministrable() && false )
 {
