5e8d949ed99a370ef9db1202f68c309c99b7898d
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2012-09-12 07:31:34 +0000

    authentication by email
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@15650 a933e228-e664-4d3f-8241-49479697c698

--- code/vendors/joomla/plugins/authentication/joomla.php
+++ code/vendors/joomla/plugins/authentication/joomla.php
@@ -1,10 +1,10 @@
 <?php
 /**
-* @version		$Id: joomla.php 14401 2010-01-26 14:10:00Z louis $
-* @package		Joomla
-* @subpackage	JFramework
-* @copyright	Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
+* @version      $Id: joomla.php 14401 2010-01-26 14:10:00Z louis $
+* @package      Joomla
+* @subpackage   JFramework
+* @copyright    Copyright (C) 2005 - 2010 Open Source Matters. All rights reserved.
+* @license      GNU/GPL, see LICENSE.php
 * Joomla! is free software. This version may have been modified pursuant
 * to the GNU General Public License, and as distributed it includes or
 * is derivative of works licensed under the GNU General Public License or
@@ -20,86 +20,94 @@ jimport( 'joomla.plugin.plugin' );
 /**
  * Joomla Authentication plugin
  *
- * @package		Joomla
- * @subpackage	JFramework
+ * @package     Joomla
+ * @subpackage  JFramework
  * @since 1.5
  */
 class plgAuthenticationJoomla extends JPlugin
 {
 
-	/**
-	 * Constructor
-	 *
-	 * For php4 compatability we must not use the __constructor as a constructor for plugins
-	 * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-	 * This causes problems with cross-referencing necessary for the observer design pattern.
-	 *
-	 * @param object $subject The object to observe
-	 * @param array  $config  An array that holds the plugin configuration
-	 * @since 1.5
-	 */
-	function plgAuthenticationJoomla(& $subject, $config) {
-		parent::__construct($subject, $config);
-	}
+    /**
+     * Constructor
+     *
+     * For php4 compatability we must not use the __constructor as a constructor for plugins
+     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
+     * This causes problems with cross-referencing necessary for the observer design pattern.
+     *
+     * @param object $subject The object to observe
+     * @param array  $config  An array that holds the plugin configuration
+     * @since 1.5
+     */
+    function plgAuthenticationJoomla(& $subject, $config) {
+        parent::__construct($subject, $config);
+    }
 
-	/**
-	 * This method should handle any authentication and report back to the subject
-	 *
-	 * @access	public
-	 * @param   array 	$credentials Array holding the user credentials
-	 * @param 	array   $options     Array of extra options
-	 * @param	object	$response	 Authentication response object
-	 * @return	boolean
-	 * @since 1.5
-	 */
-	function onAuthenticate( $credentials, $options, &$response )
-	{
-		jimport('joomla.user.helper');
+    /**
+     * This method should handle any authentication and report back to the subject
+     *
+     * @access  public
+     * @param   array   $credentials Array holding the user credentials
+     * @param   array   $options     Array of extra options
+     * @param   object  $response    Authentication response object
+     * @return  boolean
+     * @since 1.5
+     */
+    function onAuthenticate( &$credentials, $options, &$response )
+    {
+        jimport('joomla.user.helper');
 
-		// Joomla does not like blank passwords
-		if (empty($credentials['password']))
-		{
-			$response->status = JAUTHENTICATE_STATUS_FAILURE;
-			$response->error_message = 'Empty password not allowed';
-			return false;
-		}
+        // Joomla does not like blank passwords
+        if (empty($credentials['password']))
+        {
+            $response->status = JAUTHENTICATE_STATUS_FAILURE;
+            $response->error_message = 'Empty password not allowed';
+            return false;
+        }
 
-		// Initialize variables
-		$conditions = '';
+        // Initialize variables
+        $conditions = '';
+        
+        // Get a database object
+        $db =& JFactory::getDBO();
+        $username   = $db->Quote( $credentials['username'] );
+        $query = 'SELECT `username`,`id`, `password`, `gid`'
+            . ' FROM `#__users`'
+            . ' WHERE username=' . $username
+            ;
+        //if an email
+        if ( strpos($username,'@') ) {
+            $query .= ' OR email='.$username;
+        }
+        
+        $db->setQuery( $query );
+        
+        $result = $db->loadObject();
 
-		// Get a database object
-		$db =& JFactory::getDBO();
 
-		$query = 'SELECT `id`, `password`, `gid`'
-			. ' FROM `#__users`'
-			. ' WHERE username=' . $db->Quote( $credentials['username'] )
-			;
-		$db->setQuery( $query );
-		$result = $db->loadObject();
+        if($result)
+        {
+            //if login with email then set the username credential
+            $credentials['username'] = $result->username;           
+            $parts  = explode( ':', $result->password );
+            $crypt  = $parts[0];
+            $salt   = @$parts[1];
+            $testcrypt = JUserHelper::getCryptedPassword($credentials['password'], $salt);
 
-
-		if($result)
-		{
-			$parts	= explode( ':', $result->password );
-			$crypt	= $parts[0];
-			$salt	= @$parts[1];
-			$testcrypt = JUserHelper::getCryptedPassword($credentials['password'], $salt);
-
-			if ($crypt == $testcrypt) {
-				$user = JUser::getInstance($result->id); // Bring this in line with the rest of the system
-				$response->email = $user->email;
-				$response->fullname = $user->name;
-				$response->status = JAUTHENTICATE_STATUS_SUCCESS;
-				$response->error_message = '';
-			} else {
-				$response->status = JAUTHENTICATE_STATUS_FAILURE;
-				$response->error_message = 'Invalid password';
-			}
-		}
-		else
-		{
-			$response->status = JAUTHENTICATE_STATUS_FAILURE;
-			$response->error_message = 'User does not exist';
-		}
-	}
+            if ($crypt == $testcrypt) {             
+                $user = JUser::getInstance($result->id); // Bring this in line with the rest of the system
+                $response->email = $user->email;
+                $response->fullname = $user->name;
+                $response->status = JAUTHENTICATE_STATUS_SUCCESS;
+                $response->error_message = '';
+            } else {
+                $response->status = JAUTHENTICATE_STATUS_FAILURE;
+                $response->error_message = 'Invalid password';
+            }
+        }
+        else
+        {
+            $response->status = JAUTHENTICATE_STATUS_FAILURE;
+            $response->error_message = 'User does not exist';
+        }
+    }
 }
