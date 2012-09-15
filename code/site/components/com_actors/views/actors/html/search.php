cf6008003ca1fe06f4693e61449ec157baa527d3
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2012-09-15 08:18:31 +0000

    The search layout was missing.
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@15663 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_actors/views/actors/html/search.php
+++ code/site/components/com_actors/views/actors/html/search.php
@@ -1,1 +1,7 @@
 <?php defined('KOOWA') or die; ?>
+
+<module position="sidebar-b" style="none"></module>
+
+<?= @helper('ui.searchbox', @route('layout=list'))?>
+
+<div id="an-entities-wrapper"></div>
