01cf4e436c8ff78e9b035b7cf54fc517e6b0b836
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2012-09-16 01:07:33 +0000

    the actor search handles the search
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@15688 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_people/views/people/html/search.php
+++
@@ -1,7 +0,0 @@
-<?php defined('KOOWA') or die; ?>
-
-<module position="sidebar-b" style="none"></module>
-
-<?= @helper('ui.searchbox', @route('view=people&layout=list'))?>
-
-<div id="an-entities-wrapper" class="an-entities-wrapper"></div>
