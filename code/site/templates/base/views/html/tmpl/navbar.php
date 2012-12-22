124de019ed9671b9d202d7960f8da9507e5e18f6
rmdstudio <rmdstudio@a933e228-e664-4d3f-8241-49479697c698>
2012-12-22 05:22:30 +0000

    if mobile module field is empty then the responsive mobile menu elements are not being created
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@16333 a933e228-e664-4d3f-8241-49479697c698

--- code/site/templates/base/views/html/tmpl/navbar.php
+++ code/site/templates/base/views/html/tmpl/navbar.php
@@ -1,13 +1,17 @@
 <?php defined('KOOWA') or die;?>
 
+<?php $mobile_nav = @helper('modules.render','mobile', array('style'=>'none')); ?>
+
 <div class="navbar <?= ($this->getView()->getParams()->navbarInverse) ? 'navbar-inverse' : '' ?> navbar-fixed-top">
     <div class="navbar-inner">
         <div class="container">
+        	<?php if($mobile_nav): ?>
         	<a type="button" class="btn btn-navbar" data-trigger="ShowMainmenu">
         		<span class="icon-bar"></span>
         		<span class="icon-bar"></span>
         		<span class="icon-bar"></span>
         	</a>
+        	<?php endif; ?>
         	
             <?= @render('logo') ?>
             <div id="desktop-main-menu" class="nav-collapse collapse">
@@ -17,22 +21,21 @@
 	            <?php endif; ?>
             </div>
             
+            <?php if($mobile_nav): ?>
             <div id="mobile-main-menu" class="hidden-desktop">
-            <?php if( $mobile_nav = @helper('modules.render','mobile', array('style'=>'none'))): ?>
             <?= $mobile_nav ?>
-            <?php endif; ?>
             </div>
+            <script>
+            var mobileMenuToggle = new Fx.Slide(document.getElement('#mobile-main-menu ul')).hide();
+			
+			Delegator.register('click', {
+				'ShowMainmenu' : function(event, el, api) {
+					event.stop();
+					mobileMenuToggle.toggle();
+				},
+			});
+			</script>
+            <?php endif; ?>
         </div>
     </div>            
 </div>
-
-<script>
-var mobileMenuToggle = new Fx.Slide(document.getElement('#mobile-main-menu ul')).hide();
-
-Delegator.register('click', {
-	'ShowMainmenu' : function(event, el, api) {
-		event.stop();
-		mobileMenuToggle.toggle();
-	},
-});
-</script>
