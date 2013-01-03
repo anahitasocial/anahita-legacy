86566d45dac19173c18ab076c5fc214e7b442cb0
asanieyan <asanieyan@a933e228-e664-4d3f-8241-49479697c698>
2013-01-03 21:57:49 +0000

    searchbox updates the main-entity parent
    git-svn-id: file:///var/www/svn/anahita/anahita/trunk@16414 a933e228-e664-4d3f-8241-49479697c698

--- code/site/components/com_base/templates/helpers/ui/_ui_searchbox.php
+++ code/site/components/com_base/templates/helpers/ui/_ui_searchbox.php
@@ -1,7 +1,7 @@
 <?php defined('KOOWA') or die; ?>
 <form id="an-search-form" class="well form-search" name="an-search-form"  method="GET">				
 	<input  type="text" name="q" class="input-large search-query" id="an-search-query" value="" size="21" maxlength="21" />
-	<button data-trigger="Request" data-request-options="{url:'<?=$search_action?>',update:'an-entities-main'}" type="submit" class="btn">
+	<button data-trigger="Request" data-request-options="{url:'<?=$search_action?>',update:'#an-entities-main ! div'}" type="submit" class="btn">
 		<?=@text('LIB-AN-ACTION-SEARCH')?>
 	</button>
 </form>
